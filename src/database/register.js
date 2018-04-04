8b1a35b7c31653c15a8346fdd7d5233d8ad807e2
Knarfux <franck.hild@gmail.com>
2018-04-04 17:31:40 +0200

    Register new user into database

--- src/database/register.js
+++ src/database/register.js
@@ -1,3 +1,30 @@
-const register = () => {}
+const hash = require('hash.js')
+
+const logger = require('../logger')
+
+const salt = "superjesus"
+
+const register = db => ({ username, password }) => {
+	if (!username || !password) {
+		const message = "Incorrect login format"
+		logger.error(message)
+		return { validation: false, message }
+	}
+	return db.hlenAsync(username).then(number => {
+		if (number == 0) {
+			const message = "Username already exist"
+			logger.error(message)
+			return { validation: false, message }
+		}
+		return db.hmsetAsync(username,
+			"username", username,
+			"password", hash.sha512(`${salt}:${password}`)
+		).then(res => {
+			const message = "User created ! :)"
+			logger.info(message)
+			return { validation: true, message }
+		})
+	})
+}
 
 module.exports = register
--- src/routes/registerRoute.js
+++ src/routes/registerRoute.js
@@ -1,7 +1,7 @@
 const database = require('../database')
 
 const registerRoute = app => app.post('/register', ({ body }, res) => {
-	database.then(db => db.auth(body)).then(res.json)
+	database.then(db => db.register(body)).then(res.json)
 })
 
 module.exports = registerRoute
